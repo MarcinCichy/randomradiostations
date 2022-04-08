41340ad9244de43a3ed419ded886eeb51502a74d
Marcin Cichy <marcin-cichy@wp.pl>
2022-04-09 01:51:21 +0200

    Final version

--- rrs_v2.py
+++ rrs_v2.py
@@ -22,35 +22,46 @@ class Application(Frame):
 	def __init__(self, master):
 		super(Application, self).__init__(master)
 		self.btn_random = Button(root)
+		self.btn_random_clicks = 0
 		self.pack()
 		self.create_widgets()
 		
 	def create_widgets(self):
 		# create button for the draw
-		self.btn_random['text'] = "LOSOWANIE"
+		self.btn_random['text'] = "LOSOWANIE \n Naciśnij mnie :-)"
 		self.btn_random['image'] = None
 		self.btn_random['height'] = "160"
 		self.btn_random['width'] = "160"
-		self.btn_random['command'] = self.choose_radio
-		self.btn_random.pack()
-	
+		self.btn_random['command'] = self.update_clicks
+		self.btn_random.pack(fill='both')
+
+	def update_clicks(self):
+		self.btn_random_clicks += 1
+		print(self.btn_random_clicks)
+		self.change_img()
+
+	def change_img(self):
+		self.choose_radio()
+		if self.btn_random_clicks % 2 != 0:
+			self.after(100, self.change_img)
+		elif self.btn_random_clicks % 2 == 0:
+			self.after_cancel(self.btn_random)
+			self.btn_random['image'] = image
+
 	def choose_radio(self):
 		for picture in path.glob("*.png"):
 			radios_list.append(picture)
-			
 		station = random.choice(radios_list)
 		# print(station) # to check myself
-		
 		global image
 		image = PhotoImage(file=station)
-		# self.btn_random['text'] = None
 		self.btn_random['image'] = image
 
 
 root = Tk()
-root.title('Choose A Random Radio Station')
-root.geometry('180x170')
-root.minsize(180, 170)
-root.maxsize(180, 170)
 app = Application(root)
+root.title('Choose A Random Radio Station')
+root.geometry('180x180')
+root.minsize(180, 180)
+root.maxsize(180, 180)
 root.mainloop()
